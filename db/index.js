1926e799d01ab9d96e623c7d7c3ef47265ac9415
Luis Rodriguez <luisu_10@hotmail.com>
2022-05-25 21:52:15 -0500

    added get power users

--- db/index.js
+++ db/index.js
@@ -1,76 +1,115 @@
-const Sequelize = require('sequelize')
-const {STRING, UUID, UUIDV4, VIRTUAL, ENUM} = Sequelize
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_mail_db')
+const Sequelize = require("sequelize");
+const { STRING, UUID, UUIDV4, VIRTUAL, ENUM } = Sequelize;
+const conn = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost/acme_mail_db"
+);
 
-const User = conn.define('user', {
-    id:{
-        type: UUID,
-        primaryKey: true,
-        defaultValue: UUIDV4
+const User = conn.define("user", {
+  id: {
+    type: UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4,
+  },
+  firstName: {
+    type: STRING(20),
+    allowNull: false,
+  },
+  lastName: {
+    type: STRING(20),
+    allowNull: false,
+  },
+  fullName: {
+    type: VIRTUAL,
+    get: function () {
+      return `${this.firstName} ${this.lastName}`;
     },
-    firstName: {
-        type: STRING(20),
-        allowNull: false
-    },
-    lastName: {
-        type: STRING(20),
-        allowNull: false
-    },
-    fullName: {
-        type: VIRTUAL,
-        get: function(){
-            return `${this.firstName} ${this.lastName}`
-        }
-    },
-    userLevel:{
-        type: ENUM('POWER', 'REGULAR', 'RESTRICTED'),
-        allowNull: false,
-        defaultValue: 'REGULAR'
-    }
-})
+  },
+  userLevel: {
+    type: ENUM("POWER", "REGULAR", "RESTRICTED"),
+    allowNull: false,
+    defaultValue: "REGULAR",
+  },
+});
 
-const Message = conn.define('message', {
-    id:{
-        type: UUID,
-        primaryKey: true,
-        defaultValue: UUIDV4
+User.getPowerUsers = async function () {
+  const users = await User.findAll({
+    where: {
+      userLevel: "POWER",
     },
-    subject: {
-        type: STRING(20),
-        allowNull: false
-    }
-})
+    include: [
+      {model: Message, as: "sent"},
+      {model: Message,as: "received"},
+    ]
+  });
 
-Message.belongsTo(User, {as: 'from'})
-Message.belongsTo(User, {as: 'to'})
-User.hasMany(Message, {foreignKey: 'fromId', as:'sent'})
-User.hasMany(Message, {foreignKey: 'toId', as:'received'})
+  return users;
+};
+
+// User.getPowerUsers = async function () {
+//   const users = await User.findAll({
+//     where: {
+//       userLevel: "POWER",
+//     },
+//     include: [
+//       { model: Message, as: "sent" },
+//       { model: Message, as: "received" },
+//     ],
+//   });
+//   return users
+// };
+
+const Message = conn.define("message", {
+  id: {
+    type: UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4,
+  },
+  subject: {
+    type: STRING(20),
+    allowNull: false,
+  },
+});
+
+Message.belongsTo(User, { as: "from" });
+Message.belongsTo(User, { as: "to" });
+User.hasMany(Message, { foreignKey: "fromId", as: "sent" });
+User.hasMany(Message, { foreignKey: "toId", as: "received" });
 
 const seeder = async () => {
-    await conn.sync({force: true})
-    const [moe, larry, lucy, ethyl, hi, bye, hello] = await Promise.all([
-        User.create({firstName: 'Moe', lastName: 'Green', userLevel: 'POWER'}),
-        User.create({firstName: 'Larry', lastName: 'Lubin'}),
-        User.create({firstName: 'Lucy', lastName: 'Lasser', userLevel: 'RESTRICTED'}),
-        User.create({firstName: 'Ethyl', lastName: 'Evans'}),
-        Message.create({subject: 'Hi'}),
-        Message.create({subject: 'Bye'}),
-        Message.create({subject: 'Hello'}),
-    ])
+  await conn.sync({ force: true });
+  const [moe, larry, lucy, ethyl, hi, bye, hello] = await Promise.all([
+    User.create({ firstName: "Moe", lastName: "Green", userLevel: "POWER" }),
+    User.create({
+      firstName: "Larry",
+      lastName: "Lubin",
+    }),
+    User.create({
+      firstName: "Lucy",
+      lastName: "Lasser",
+      userLevel: "RESTRICTED",
+    }),
+    User.create({
+      firstName: "Ethyl",
+      lastName: "Evans",
+    }),
+    Message.create({ subject: "Hi" }),
+    Message.create({ subject: "Bye" }),
+    Message.create({ subject: "Hello" }),
+  ]);
 
-    hi.fromId = moe.id
-    hi.toId = lucy.id
-    await hi.save()
-    bye.fromId = lucy.id
-    bye.toId = moe.id
-    await bye.save()
-    hello.fromId = ethyl.id
-    hello.toId = larry.id
-    await hello.save()
-}
+  hi.fromId = moe.id;
+  hi.toId = lucy.id;
+  await hi.save();
+  bye.fromId = lucy.id;
+  bye.toId = moe.id;
+  await bye.save();
+  hello.fromId = ethyl.id;
+  hello.toId = larry.id;
+  await hello.save();
+};
 
 module.exports = {
-    User,
-    Message,
-    seeder
-}
+  User,
+  Message,
+  seeder,
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const { User, Message, seeder } = require("./db");
 
 app.use('/assets', express.static('assets'))
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', async (req, res, next) => {
     try {
@@ -22,6 +23,7 @@ app.get('/', async (req, res, next) => {
                         <a href='/users'>Users</a>
                         <a href='/messages'>Messages</a>
                     </nav>
+                    
                     <p>
                         We have ${users.length} users and we have ${messages.length}!!!
                     </p>
@@ -59,7 +61,62 @@ app.get('/users', async (req, res, next) => {
                         <a href='/'>Home</a>
                         <a href='/users' class='select'>Users</a>
                         <a href='/messages'>Messages</a>
+                        
                     </nav>
+                    <a href='/users/power'>Filter Power Users</a>
+                    <ul>
+                    ${users.map( user =>   `
+                        <li>
+                            ${user.fullName} (${user.userLevel})
+                            <div>
+                                Sent ${user.sent.length} messages
+                                <br/>
+                                Received ${user.received.length} messages
+                            </div>
+                        </li>
+                    `).join('')}
+                    </ul>
+                </body>
+            </html>
+        `);
+    } catch (ex) {
+        next(ex)
+    }
+})
+
+app.get('/users/power', async (req, res, next) => {
+    try {
+        // const users= await User.findAll({
+        //     include: [
+        //         {
+        //             model: Message,
+        //             as: 'sent'
+        //         },
+        //         {
+        //             model: Message,
+        //             as: 'received'
+        //         }
+        //     ]
+            
+            
+        // });
+
+        const users = await User.getPowerUsers()
+
+        res.send(`
+            <html>
+                <head>
+                    <title>Acme Mail</title>
+                    <link rel='stylesheet' href='/assets/styles.css'/>
+                </head>
+                <body>
+                    <nav>
+                        <a href='/'>Home</a>
+                        <a href='/users' class='select'>Users</a>
+                        <a href='/messages'>Messages</a>
+                        
+                    </nav>
+                    <a href='/users/power'>Filter Power Users</a>
                     <ul>
                     ${users.map( user =>   `
                         <li>
@@ -97,6 +154,8 @@ app.get('/messages', async (req, res, next) => {
                 ]
             }
         );
+
+        const users = await User.findAll()
         
         // res.send(messages)
 
@@ -112,6 +171,22 @@ app.get('/messages', async (req, res, next) => {
                         <a href='/users'>Users</a>
                         <a href='/messages' class='select'>Messages</a>
                     </nav>
+                    <form method='POST'>
+                        <select name='fromId'>
+                        <option>-- From Id</option>
+                            ${users.map(user => `
+                                <option value='${user.id}'>${user.fullName}</option>
+                            `).join('')}
+                        </select>
+                        <select name='toId'>
+                        <option>-- To Id</option>
+                            ${users.map(user => `
+                                <option value='${user.id}'>${user.fullName}</option>
+                            `).join('')}
+                        </select>
+                        <input type='text' name='subject' placeHolder='Subject'/>
+                        <button>Create</button>
+                    </from>
                     <ul>
                     ${messages.map( message =>   `
                         <li>
@@ -125,6 +200,19 @@ app.get('/messages', async (req, res, next) => {
     } catch (ex) {
         next(ex)
     }
+})
+
+app.use('/messages', async(req, res, next) => {
+    try {
+        await Message.create(req.body)
+        res.redirect('/messages')
+    } catch (ex) {
+        next(ex)
+    }
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).send(err.message)
 })
 
 const setupDB = async () => {
